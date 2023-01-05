d8ae3a1f1c4ab2fdc96dfcbb2bc5f12ba5c5bc3b
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-01-04 21:15:56 -0300

    feat: new events handled by the sockets server

--- src/socket/handlers/admin/index.ts
+++ src/socket/handlers/admin/index.ts
@@ -1,7 +1,7 @@
 import { Namespace, Server, Socket } from 'socket.io'
-import { EVENTS } from '../../../config/data/events'
+import { CODE, EVENTS } from '../../../config/data/events'
 
-import { THandler } from '../../../config/types/customTypes'
+import { THandler, TRoom, TRoomInfo, TUser } from '../../../config/types/customTypes'
 import { Room } from '../../../database/db'
 import { Logger } from '../../../helpers/logger'
 
@@ -19,24 +19,39 @@ class AdminHandler implements THandler {
             this.socket = socket
             Logger.info(`admin ${socket.id} connected`)
 
-            socket.on(EVENTS.DISCONNECT, (reason) => this.disconnect(reason))
+            socket.on(EVENTS.DISCONNECT, (reason) => this.onDisconnect(reason))
             socket.on(EVENTS.ADMIN.CONNECT_ERROR, (err) => {
                 Logger.error(`connect_error due to ${err.message}`)
             })
 
-            socket.on(EVENTS.ADMIN.START, () => this.onStart())
+            socket.on(EVENTS.ADMIN.START_VIDEO, () => this.onStart())
             socket.on(EVENTS.ADMIN.BRADCASTER, (payload) => this.onBroadcaster(payload))
             socket.on(EVENTS.ADMIN.OFFER, (payload) => this.onOffer(payload))
             socket.on(EVENTS.ADMIN.CANDIDATE, (payload) => this.onCandidate(payload))
             socket.on(EVENTS.ADMIN.END, () => this.onEnd())
+
+            socket.on(EVENTS.ADMIN.CREATE_ROOM, (payload, cb) => this.onCreateRoom(payload))
+            socket.on(EVENTS.ADMIN.GET_ROOMS, (payload, cb) => this.onGetRooms(payload, cb))
+            socket.on(EVENTS.ADMIN.SAVE_ROOMS, () => this.onSaveRooms())
+            socket.on(EVENTS.ADMIN.UPDATE_DESCRIPTION_IMAGE, (payload) => this.onUpdateDescriptionAndImage(payload))
+            socket.on(EVENTS.ADMIN.TOGGLE_AVAILABLE, (payload, cb) => this.onToggleAvailable(payload, cb))
+            socket.on(EVENTS.ADMIN.DELETE_ROOM, (payload, cb) => this.onDeleteRoom(payload, cb))
+
+            this.updateRooms()
         }) 
     }
 
-    disconnect(reason: string) {
+    onDisconnect(reason: string) {
+        if (!this.socket) return
+
         Logger.info(`admin ${this.socket?.id} disconnected - reason: ${reason}`)
-        // users.delete(this.socket!)
+        const _rooms = this.getRoomsOfBroadcaster(this.socket.id)
+        if (!_rooms) return
+
+        this.deleteSpecificRooms(_rooms)
+
         this.io.of(EVENTS.NAMESPACE.CLIENT)
-        .to('myRoom')
+        .to(_rooms)
         .emit(EVENTS.DISCONNECT_PEER)
     }
 
@@ -48,38 +63,155 @@ class AdminHandler implements THandler {
         //     room: room,
         //     user: user
         // })
-        // this.socket.join(room)
+        this.socket.join(room)
 
         this.io.of(EVENTS.NAMESPACE.CLIENT)
         .emit(EVENTS.CLIENT.EMIT.BROADCASTER)
     }
 
+    // after onWatcher
     onOffer({ socketClientId, adminLocalDescription }: { socketClientId: string, adminLocalDescription: string }) {
         if (!this.socket) return
         this.io.of(EVENTS.NAMESPACE.CLIENT)
             .to(socketClientId)
-            .emit(EVENTS.CLIENT.EMIT.OFFER, 
-                  this.socket.id,
-                  adminLocalDescription)
+            .emit(EVENTS.CLIENT.EMIT.OFFER, {
+                adminSocketId: this.socket.id,
+                remoteDescription: adminLocalDescription
+            })
     }
 
-    onCandidate({ id, message }: { id: string, message: string }) {
+    // after onWatcher
+    onCandidate({ clientSocketId, eventCandidate }: { clientSocketId: string, eventCandidate: string }) {
         if (!this.socket) return
         this.io.of(EVENTS.NAMESPACE.CLIENT)
-        .to(id)
-        .emit(EVENTS.CLIENT.EMIT.CANDIDATE,
-              this.socket.id, 
-              message)
+        .to(clientSocketId)
+        .emit(EVENTS.CLIENT.EMIT.CANDIDATE, {
+            adminSocketId: this.socket.id,
+            candidate: eventCandidate
+        })
     }
 
     onStart() {
-        this.socket?.emit(EVENTS.ADMIN.START)
+        if (!this.socket) return 
+        this.socket.emit(EVENTS.ADMIN.START_VIDEO)
     }
 
     onEnd() {
         // get the connection which ended
         this.io.of(EVENTS.NAMESPACE.CLIENT)
         .emit(EVENTS.CLIENT.EMIT.END_BROADCAST)
+    }
+
+    onCreateRoom(room: TRoom) {
+        if (!this.socket) return
+        // check if room already exists
+        if (Room.rooms.get(room.room.roomName)) // emit back to sender
+            return this.socket.emit(EVENTS.ADMIN.ROOM_CREATED, {
+                roomCode: CODE.ROOM.ALREADY_EXISTS,
+                room: null
+            })
+        // create room
+        Room.rooms.set(room.room.roomName, room)
+        this.socket.emit(EVENTS.ADMIN.ROOM_CREATED, {
+            roomCode: CODE.ROOM.OK,
+            room: room
+        })
+        // send to client
+        const roomsArray = this.getAllRooms()
+        this.io.of(EVENTS.NAMESPACE.CLIENT)
+        .emit(EVENTS.ADMIN.CREATE_ROOM, {
+            rooms: roomsArray
+        })
+    }
+
+    onGetRooms({ broadcasterName }: { broadcasterName: string }, callback: Function) {
+        const roomsArray: TRoom[] = []
+        const roomsNames = this.getRoomsOfBroadcaster(broadcasterName)
+        roomsNames.forEach((roomName) => {
+            const room = Room.rooms.get(roomName)
+            if (room) roomsArray.push(room)
+        })
+        callback(roomsArray)
+    }
+
+    onSaveRooms() {
+        Room.save()
+    }
+
+    onUpdateDescriptionAndImage({ room, description, image }: { room: TRoom, description: string, image: string }) {
+        Room.rooms.set(room.room.roomName, {
+            broadcaster: {
+                name: room.broadcaster.name,
+                socketId: room.broadcaster.socketId
+            },
+            room: {
+                roomName: room.room.roomName,
+                title: description,
+                preview: image,
+                ready: true,
+                available: false
+            },
+            user: null
+        })
+        this.socket?.emit(EVENTS.ADMIN.READY, { room: Room.rooms.get(room.room.roomName) })
+        // send to client
+        const roomsArray = this.getAllRooms()
+        this.io.of(EVENTS.NAMESPACE.CLIENT)
+        .emit(EVENTS.ADMIN.UPDATE_DESCRIPTION_IMAGE, {
+            rooms: roomsArray
+        })
+    }
+
+    onToggleAvailable({ availableRoom }: { availableRoom : TRoom }, callback: Function) {
+        Room.rooms.set(availableRoom.room.roomName, availableRoom)
+        callback(availableRoom)
+        // send to client
+        const roomsArray = this.getAllRooms()
+        this.io.of(EVENTS.NAMESPACE.CLIENT)
+        .emit(EVENTS.ADMIN.UPDATE_DESCRIPTION_IMAGE, {
+            rooms: roomsArray
+        })
+    }
+
+    onDeleteRoom({ iRoom }: { iRoom: TRoom }, callback: Function) {
+        Room.rooms.delete(iRoom.room.roomName)
+        const rooms = this.getAllRooms()
+        callback(rooms)
+        // send to client
+        this.io.of(EVENTS.NAMESPACE.CLIENT)
+        .emit(EVENTS.ADMIN.DELETE_ROOM, { rooms: rooms })
+    }
+
+    /**
+     * update rooms variable to saved config in file
+     */
+    private updateRooms() {
+        const fileRooms = Room.read()
+        fileRooms.forEach((value, key) => { Room.rooms.set(key, value) })
+    }
+
+    private getRoomsOfBroadcaster(name: string) {
+        const broadcasterRooms: string[] = []
+        Room.rooms.forEach((value) => {
+            if (name === value.broadcaster.name)
+                broadcasterRooms.push(value.room.roomName)
+        })
+        return broadcasterRooms
+    }
+
+    private deleteSpecificRooms(rooms: string[]) {
+        for (const room of rooms) {
+            Room.rooms.delete(room)
+        }
+    }
+
+    private getAllRooms() {
+        const roomsArray: TRoom[] = []
+        const rooms = Room.rooms.values()
+        for (const room of rooms) {
+            roomsArray.push(room)
+        }
+        return roomsArray
     }
 }
 
--- src/socket/handlers/client/index.ts
+++ src/socket/handlers/client/index.ts
@@ -1,7 +1,8 @@
 import { Namespace, Server, Socket } from 'socket.io'
 import { Logger } from '../../../helpers/logger'
-import { THandler } from '../../../config/types/customTypes'
-import { EVENTS } from '../../../config/data/events'
+import { THandler, TRoom, TUser } from '../../../config/types/customTypes'
+import { CODE, EVENTS } from '../../../config/data/events'
+import { Room } from '../../../database/db'
 
 class ClientHandler implements THandler {
     private io: Server
@@ -26,50 +27,107 @@ class ClientHandler implements THandler {
             socket.on(EVENTS.CLIENT.ANSWER, (payload) => this.onAnswer(payload))
             socket.on(EVENTS.CLIENT.CANDIDATE, (payload) => this.onCandidate(payload))
             socket.on(EVENTS.CLIENT.END, (payload) => this.onEnd(payload))
+
+            socket.on(EVENTS.CLIENT.GET_ROOMS, (cb) => this.onGetRooms(cb))
+            socket.on(EVENTS.CLIENT.JOIN, (payload, cb) => this.onJoin(payload, cb))
         })
     }
 
     onDisconnect(reason: string) {
-        Logger.info(`user ${this.socket?.id} disconnected - reason: ${reason}`)
-        // users.delete(this.socket!)
-        // this.socket?.to('myRoom').emit('disconnectPeer', this.socket.id)
+        if (!this.socket) return
+        Logger.info(`user ${this.socket.id} disconnected - reason: ${reason}`)
+
+        const { room, broadcaster } = this.getRoomOfSocketId(this.socket.id)
+
+        if(!room) return
+
+        Room.rooms.set(room.roomName, {
+            broadcaster: broadcaster,
+            room: room,
+            user: null
+        })
+
         this.io.of(EVENTS.NAMESPACE.ADMIN)
-            // .to('myRoom')
-            .emit(EVENTS.DISCONNECT_PEER,
-                this.socket?.id)
+            .to(broadcaster.socketId)
+            .emit(EVENTS.DISCONNECT_PEER, {
+                clientSocketId: this.socket.id
+            })
     }
 
+    // after join
     onWatcher({ room }: { room: string }) {
         if (!this.socket) return
-        // Room.instance.rooms.get(room)
-        // const broadcast = { broadcaster: 'admin' }
+        // get room
+        const _room = Room.rooms.get(room)
+        // emit event to broadcaster
         this.io.of(EVENTS.NAMESPACE.ADMIN)
-        // .to(broadcast.broadcaster)
-        .emit(EVENTS.ADMIN.EMIT.WATCHER,
-              this.socket.id)
+        .to(_room?.broadcaster.socketId!)
+        .emit(EVENTS.ADMIN.EMIT.WATCHER, {
+            clientSocketId: this.socket.id
+        })
     }
 
+    // after offer
     onAnswer({ adminSocketId, clientLocalDescription }: { adminSocketId: string, clientLocalDescription: string }) {
         if (!this.socket) return
         this.io.of(EVENTS.NAMESPACE.ADMIN)
         .to(adminSocketId)
-        .emit(EVENTS.ADMIN.EMIT.ANSWER,
-              this.socket.id,
-              clientLocalDescription)
+        .emit(EVENTS.ADMIN.EMIT.ANSWER, {
+            clientSocketId: this.socket.id,
+            remoteDescription: clientLocalDescription
+        })
     }
 
-    onCandidate({ id, message }: { id: string, message: string }) {
+    // after offer
+    onCandidate({ adminSocketId, eventCandidate }: { adminSocketId: string, eventCandidate: string }) {
         if (!this.socket) return
         this.io.of(EVENTS.NAMESPACE.ADMIN)
-        .to(id)
-        .emit(EVENTS.ADMIN.EMIT.CANDIDATE,
-              this.socket.id,
-              message)
+        .to(adminSocketId)
+        .emit(EVENTS.ADMIN.EMIT.CANDIDATE, {
+            adminSocketId: this.socket.id,
+            candidate: eventCandidate
+        })
     }
 
     onEnd({ room }: { room: string }) {
         // Room.instance.rooms.delete(room)
         // this.io.to(room).emit('end-broadcast')
+    }
+
+    onGetRooms(callback: Function) {
+        const roomsArray: TRoom[] = []
+        const rooms = Room.rooms.values()
+        for (const room of rooms) {
+            roomsArray.push(room)
+        }
+        callback(roomsArray)
+    }
+
+    onJoin({ room, user }: { room: string, user: TUser }, callback: Function) {
+        if (!this.socket) return callback(CODE.SOCKET.DOESNT_EXISTS)
+        // check if room exists
+        const _room = Room.rooms.get(room)
+        if (!_room)
+            return callback(CODE.ROOM.DOESNT_EXISTS)
+        // check if room not empty
+        if (_room.user)
+            return callback(CODE.ROOM.NOT_EMPTY)
+        // update and join room
+        Room.rooms.set(room, { 
+            broadcaster: _room.broadcaster,
+            room: _room.room,
+            user: user
+        })
+        this.socket.join(room)
+    }
+
+    private getRoomOfSocketId(socketId: string) {
+        const values = Room.rooms.values()
+        for (let value of values) {
+            if (socketId === value.user?.socketId)
+                return { room: value.room, broadcaster: value.broadcaster }
+        }
+        return { room: null, broadcaster: null }
     }
 }
 
