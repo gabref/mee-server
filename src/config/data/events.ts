3fbd94658c21878405c0a5dccf3884bec0cad6cb
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-01-04 21:12:08 -0300

    feat: add events names for sockets

--- src/config/data/events.ts
+++ src/config/data/events.ts
@@ -9,7 +9,7 @@ const EVENTS = {
     },
     ADMIN: {
         CONNECT_ERROR: 'admin:connect_error',
-        START: 'admin:start',
+        START_VIDEO: 'admin:start-video',
         BRADCASTER: 'admin:broadcaster',
         OFFER: 'admin:offer',
         CANDIDATE: 'admin:candidate',
@@ -18,7 +18,15 @@ const EVENTS = {
             WATCHER: 'admin:watcher',
             ANSWER: 'admin:answer',
             CANDIDATE: 'admin:candidate',
-        }
+        },
+        CREATE_ROOM: 'admin:create-room',
+        DELETE_ROOM: 'admin:delete-room',
+        ROOM_CREATED: 'admin:room-created',
+        SAVE_ROOMS: 'admin:save-rooms',
+        GET_ROOMS: 'admin:get-rooms',
+        UPDATE_DESCRIPTION_IMAGE: 'admin:update-description-image',
+        READY: 'admin:ready',
+        TOGGLE_AVAILABLE: 'admin:make-available'
     },
     CLIENT: {
         CONNECT_ERROR: 'client:connect_error',
@@ -27,13 +35,28 @@ const EVENTS = {
         ANSWER: 'client:answer',
         CANDIDATE: 'client:candidate',
         END: 'client:end',
+        JOIN: 'client:join',
         EMIT: {
             BROADCASTER: 'client:broadcaster',
             OFFER: 'client:offer',
             CANDIDATE: 'client:candidate',
             END_BROADCAST: 'client:end-broadcast'
-        }
+        },
+        GET_ROOMS: 'client:get-rooms',
+    },
+}
+
+const CODE = {
+    ROOM: {
+        OK: 0,
+        DOESNT_EXISTS: -1,
+        ALREADY_EXISTS: -2,
+        NOT_EMPTY: -3,
+    },
+    SOCKET: {
+        OK: 0,
+        DOESNT_EXISTS: -4,
     }
 }
 
-export { EVENTS }
+export { EVENTS, CODE }
