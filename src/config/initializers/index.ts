684692297060703dabd187350cd6f89270cbb130
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-02-28 14:16:22 -0300

    feat: dotenv for environment variables

--- src/config/initializers/index.ts
+++ src/config/initializers/index.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 // import { Logger } from '../../helpers/logger'
 import { Server } from 'http'
+import dotenv from 'dotenv'
 
 import useMiddlewares from './middlewares'
 import useSocket from './socket'
 import useRoutes from './routes'
 
 const init = (app?: express.Application): Server => {
+    dotenv.config()
+
     if (!app) throw new Error('No Express Server Application, could not start')
 
     useMiddlewares(app)
